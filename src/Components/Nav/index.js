4de534bae3d1f2391646dd617f3319b3375571c5
niishaaant <nishant98680042@gmail.com>
2023-04-12 01:11:28 +0530

    fixed resposiveness and added mobile navbar

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,41 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.scss";
 import { Link } from "react-router-dom";
+import close from "../../utils/icons8-close-window-50.png";
+import open from "../../utils/icons8-menu-50.png";
 
 const Nav = () => {
+  const [openNav, setOpenNav] = useState(false);
   return (
-    <div className="nav-container">
-      <div className="logo-container">
-        <Link to="/" className="link">
-          L2
-        </Link>
+    <>
+      <div className="nav-container">
+        <button
+          onClick={() => {
+            setOpenNav(true);
+          }}
+          className="openNavbar"
+        >
+          <img src={open} width="20px" height="20px" />
+        </button>
+        <div className="logo-container">
+          <Link to="/" className="link">
+            L2
+          </Link>
+        </div>
+        <div className="link-container">
+          <Link to="/" className="link">
+            <div className="link-item">HOME</div>
+          </Link>
+          <Link to="/roadmap" className="link">
+            <div className="link-item">ROADMAP</div>
+          </Link>
+          <div className="link-item">
+            <Link to="/collection" className="link">
+              COLLECTION
+            </Link>
+          </div>
+          <div className="link-item">
+            <Link to="/buynft" className="link">
+              BUY NFT
+            </Link>
+          </div>
+          <div className="link-item">
+            <Link to="/teamsupport" className="link">
+              TEAM | SUPPORT
+            </Link>
+          </div>
+        </div>
+        <div className="action-container">
+          <button disabled={true}>Connect Wallet</button>
+        </div>
       </div>
-      <div className="link-container">
-        <Link to="/" className="link">
+      <ResponsiveNav openNav={openNav} setOpenNav={setOpenNav} />
+    </>
+  );
+};
+
+const ResponsiveNav = ({ openNav, setOpenNav }) => {
+  return (
+    <div className={`responsive-nav-container ${openNav ? "active" : ""}`}>
+      <div className="close">
+        <button
+          className="openNavbar"
+          onClick={() => {
+            setOpenNav(false);
+          }}
+        >
+          <img src={close} width="40px" height="40px" />
+        </button>
+      </div>
+      <div className="links-container">
+        <Link
+          to="/"
+          className="link"
+          onClick={() => {
+            setOpenNav(false);
+          }}
+        >
           <div className="link-item">HOME</div>
         </Link>
-        <Link to="/roadmap" className="link">
+        <Link
+          to="/roadmap"
+          className="link"
+          onClick={() => {
+            setOpenNav(false);
+          }}
+        >
           <div className="link-item">ROADMAP</div>
         </Link>
         <div className="link-item">
-          <Link to="/collection" className="link">
+          <Link
+            to="/collection"
+            className="link"
+            onClick={() => {
+              setOpenNav(false);
+            }}
+          >
             COLLECTION
           </Link>
         </div>
         <div className="link-item">
-          <Link to="/buynft" className="link">
+          <Link
+            to="/buynft"
+            className="link"
+            onClick={() => {
+              setOpenNav(false);
+            }}
+          >
             BUY NFT
           </Link>
         </div>
-        <div className="link-item">
+        <div
+          className="link-item"
+          onClick={() => {
+            setOpenNav(false);
+          }}
+        >
           <Link to="/teamsupport" className="link">
             TEAM | SUPPORT
           </Link>
         </div>
       </div>
-      <div className="action-container">
-        <button disabled={true}>Connect Wallet</button>
-      </div>
+      <div className="social-container"></div>
     </div>
   );
 };
